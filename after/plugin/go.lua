dbfcb0d4985398df9752a10a0fed11ae58b763e4
Christopher Phillips <christopher.phillips@anchore.com>
2023-03-15 16:06:03 -0400

    chore: update go local import
    Signed-off-by: Christopher Phillips <christopher.phillips@anchore.com>

--- after/plugin/go.lua
+++ after/plugin/go.lua
@@ -1,10 +1,12 @@
 local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
 vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*.go",
-  callback = function()
-   require('go.format').goimport()
-  end,
-  group = format_sync_grp,
+    pattern = "*.go",
+    callback = function()
+        require('go.format').goimport('-local github.com/anchore')
+    end,
+    group = format_sync_grp,
 })
 
-require('go').setup()
+require('go').setup({
+    goimport = "goimports",
+})
