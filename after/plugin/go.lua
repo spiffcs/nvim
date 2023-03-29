d8ce2c384614801bc8ce8c5b24dc9b73dd5a38e9
Christopher Phillips <christopher.phillips@anchore.com>
2023-03-29 11:31:15 -0400

    feat: add copilot updates
    Signed-off-by: Christopher Phillips <christopher.phillips@anchore.com>

--- after/plugin/go.lua
+++ after/plugin/go.lua
@@ -2,7 +2,7 @@ local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
 vim.api.nvim_create_autocmd("BufWritePre", {
     pattern = "*.go",
     callback = function()
-        require('go.format').goimport('-local github.com/anchore')
+        require('go.format').goimport()
     end,
     group = format_sync_grp,
 })
--- lua/spiffcs/packer.lua
+++ lua/spiffcs/packer.lua
@@ -1,76 +1,82 @@
--- This file can be loaded by calling `lua require('plugins')`
+-- This file can be loaded by calling `lua require('plugins')
 -- Make sure this is from your init.vim
 
 -- Only required if you have packer configured as `opt`
 vim.cmd.packadd('packer.nvim')
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-  -- File browser 
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
+    -- File browser 
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
 
-  -- colorscheme
-  use({
-	  'rose-pine/neovim',
-	  as = 'rose-pine',
-	  config = function()
-		  require("rose-pine").setup({
-              disable_background = false,
-              disable_italics = false,
-          })
-		  vim.cmd('colorscheme rose-pine')
-	  end
-  })
+    -- colorscheme
+    use({
+        'rose-pine/neovim',
+        as = 'rose-pine',
+        config = function()
+            require("rose-pine").setup({
+                disable_background = false,
+                disable_italics = false,
+            })
+            vim.cmd('colorscheme rose-pine')
+        end
+    })
 
-  -- incremental parsing library for concrete syntax tree
-  -- https://tree-sitter.github.io/tree-sitter/
-  use( 'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    -- incremental parsing library for concrete syntax tree
+    -- https://tree-sitter.github.io/tree-sitter/
+    use( 'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 
-  -- TODO: install copilot
-  -- https://github.com/zbirenbaum/copilot.lua
+    -- TODO: install copilot
+    -- https://github.com/zbirenbaum/copilot.lua
+    use {
+        "zbirenbaum/copilot.lua",
+        cmd = "Copilot",
+        event = "InsertEnter",
+        config = function()
+            require("copilot").setup({})
+        end,
+    }
+    -- view syntax tree
+    use('nvim-treesitter/playground')
 
-  -- view syntax tree
-  use('nvim-treesitter/playground')
+    -- stab files for later
+    use('theprimeagen/harpoon')
 
-  -- stab files for later
-  use('theprimeagen/harpoon')
+    -- big undo boi
+    use ('mbbill/undotree')
 
-  -- big undo boi
-  use ('mbbill/undotree')
+    -- git but better
+    use ('tpope/vim-fugitive')
 
-  -- git but better
-  use ('tpope/vim-fugitive')
+    -- I need an LSP or I am scared
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v1.x',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {'ray-x/go.nvim'},
+            {'williamboman/mason.nvim'},           -- Optional
+            {'williamboman/mason-lspconfig.nvim'}, -- Optional
 
-  -- I need an LSP or I am scared
-  use {
-  'VonHeikemen/lsp-zero.nvim',
-  branch = 'v1.x',
-  requires = {
-    -- LSP Support
-    {'neovim/nvim-lspconfig'},             -- Required
-    {'ray-x/go.nvim'},
-    {'williamboman/mason.nvim'},           -- Optional
-    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},         -- Required
+            {'hrsh7th/cmp-nvim-lsp'},     -- Required
+            {'hrsh7th/cmp-buffer'},       -- Optional
+            {'hrsh7th/cmp-path'},         -- Optional
+            {'saadparwaiz1/cmp_luasnip'}, -- Optional
+            {'hrsh7th/cmp-nvim-lua'},     -- Optional
 
-    -- Autocompletion
-    {'hrsh7th/nvim-cmp'},         -- Required
-    {'hrsh7th/cmp-nvim-lsp'},     -- Required
-    {'hrsh7th/cmp-buffer'},       -- Optional
-    {'hrsh7th/cmp-path'},         -- Optional
-    {'saadparwaiz1/cmp_luasnip'}, -- Optional
-    {'hrsh7th/cmp-nvim-lua'},     -- Optional
-
-    -- Snippets
-    {'L3MON4D3/LuaSnip'},             -- Required
-    {'rafamadriz/friendly-snippets'}, -- Optional
-  },
-  use('ray-x/go.nvim')
-}
+            -- Snippets
+            {'L3MON4D3/LuaSnip'},             -- Required
+            {'rafamadriz/friendly-snippets'}, -- Optional
+        },
+        use('ray-x/go.nvim'),
+    }
 end)
-
